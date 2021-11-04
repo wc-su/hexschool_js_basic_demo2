aafc01b5c07beaafa019d3a25b6455bb0a35ea29
iamericsu <89247843+iamericsu@users.noreply.github.com>
2021-11-04 18:57:21 +0800

    20211104_update_1
    調整命名及RWD

--- js/all.js
+++ js/all.js
@@ -1,36 +1,157 @@
 $(document).ready(function () {
-    let windowWidth = $(window).width(); 
-
-    let tableHeader = ["作物名稱", "市場名稱", "上價", "中價", "下價", "平均價", "交易量"];
-    let dataLoadOK = false;
-    let tableData = [];
-    let filterData = [];
-    const onePageNum = 30;
-    let pageNow = 0;
-    let pageAll = 0;
-    let typeList = [
+    const categoryList = [
         { 種類: "蔬菜", 代碼: ["N04"] },
         { 種類: "水果", 代碼: ["N05"] },
         { 種類: "花卉", 代碼: ["N06", "   "] }
     ];
+    const tableHeader = ["作物名稱", "市場名稱", "上價", "中價", "下價", "平均價", "交易量"];
 
-    const list = $(".main table");
+    let dataLoadOK = false; // 資料載入是否成功
+    let tableData = []; // 載入資料
+    let filterData = []; // 篩選後資料
+    const onePageNum = 30; // 畫面一頁顯示最多筆數
+    let pageAll = 0; // 全部頁數
+    let pageNow = 0; // 目前頁數
 
-    let listHeader = `<tr class="list-header">`;
+    const table = $(".main table");
+
+    // 載入 table header
+    let headerText = `<tr class="list-header">`;
     $.each(tableHeader, function (index, value) {
         if(value == "作物名稱" || value == "市場名稱") {
-            listHeader += `<th>${value.substr(0, 2)}<span>${value.substr(2, 2)}</span></th>`;
+            headerText +=
+                `<th>${value.substr(0, 2)}<span>${value.substr(2, 2)}</span></th>`;
         } else {
-            listHeader += `<th class="th-sort">${value}`;
-            listHeader += `<i class="fas fa-sort-up"></i><i class="fas fa-sort-down"></i></th>`;
+            headerText +=
+                `<th class="th-sort">${value}<i class="fas fa-sort-up"></i><i class="fas fa-sort-down"></i></th>`;
         }
     });
-    listHeader += `</tr><tr class="list-item"><td colspan="${tableHeader.length}">請輸入並搜尋想比價的作物名稱^＿^</td></tr>`;
-    list.append(listHeader);
+    headerText += 
+        `</tr><tr class="list-item"><td colspan="${tableHeader.length}">請輸入並搜尋想比價的作物名稱^＿^</td></tr>`;
+    table.append(headerText);
 
-    $(".type li").on("click", function () {
-        $(".type li").removeClass("type-active");
-        $(this).addClass("type-active");
+    function dealData(data) {
+        tableData = data;
+        
+        let category = $(".category-active").text();
+        let cropName = $(".crop-name input[type=text]").val().trim();
+        let categoryCodeArr = categoryList.find(x => x["種類"] == category)["代碼"];
+        filterData = tableData.filter(function(item) {
+            let check= false;
+            // 對應蔬菜種類代碼
+            if(categoryCodeArr.indexOf(item["種類代碼"]) != -1) {
+                if(cropName == "") { // 未輸入作物名稱
+                    check = true;
+                } else {
+                    // 對應作物名稱
+                    if(item['作物名稱'].indexOf(cropName) != -1) {
+                        check = true;
+                    }
+                    // 清空輸入作物名稱
+                    $(".crop-name input[type=text]").val("");
+                }
+            }
+            return check;
+        });
+
+        // 計算頁數
+        pageAll = Math.ceil(filterData.length / onePageNum);
+        pageNow = pageAll > 0 ? 1 : 0;
+        // 顯示於畫面上
+        showTable("done");
+    }
+
+    function showTable(staus) {   
+        let windowWidth = $(window).width();   
+        let listItem = '';
+        $(table).find("tr").remove(".list-item");
+        if(staus == "loading") {
+            // 更新表格顯示資訊：資料載入中
+            listItem = `<tr class="list-item"><td colspan="${tableHeader.length}">資料載入中...</td></tr>`;
+            // 更新 h2 文字
+            let category = $(".category-active").text();
+            let cropName = $(".search input[type=text]").val().trim();
+            if(cropName == "") {
+                $("h2 span").text(`「${category}」的`);
+            } else {
+                $("h2 span").text(`「${cropName}」的`);
+            }
+            // $("h2 span").css("display", "block");
+            $("h2 span").addClass("display");
+        } else {
+            if(pageAll == 0) {
+                // 更新表格顯示資訊：查無資料
+                listItem = `<tr class="list-item"><td colspan="${tableHeader.length}">查詢不到當日的交易資訊QQ</td></tr>`;
+            } else {
+                let count = 0;
+                let i = (pageNow - 1) * onePageNum; // 依現行頁數計算起始位置
+                for(; i < filterData.length && count < onePageNum; i++) {
+                    listItem += `<tr class="list-item">`;
+                    // 拆解 作物名稱
+                    let cropNameArr = filterData[i]['作物名稱'].split('-');
+                    listItem += `<td class="font-bold">${cropNameArr[0]}`;
+                    if(cropNameArr[1] != undefined) {
+                        listItem += `<span>-${cropNameArr[1]}</span>`;
+                    }
+                    listItem +=
+                        `</td><td class="font-bold">${filterData[i]['市場名稱']}</td>` +
+                        `<td>${filterData[i]['上價']}</td>` +
+                        `<td>${filterData[i]['中價']}</td>` +
+                        `<td>${filterData[i]['下價']}</td>` +
+                        `<td>${filterData[i]['平均價']}</td>` +
+                        `<td>${filterData[i]['交易量']}</td></tr>`;
+                    count++;
+                }
+                showPage();
+            }
+        }
+        $(table).append(listItem);
+    }
+    
+    function showPage() {
+        // 移除所有頁數
+        $(".table-page li").remove(".page-num");
+        // 判斷顯示頁數
+        let count = 0;
+        let prevElement = $(".page-prev");
+        for(let i = -2; i < 5; i++) {
+            let nextPage = pageNow + i;
+            if(nextPage >= 1 && nextPage <= pageAll) {
+                if(nextPage == pageNow){
+                    $(`<li class="page-num page-num${nextPage} page-active" data-page=${nextPage}>${nextPage}</li>`).insertAfter($(prevElement));
+                }
+                else{
+                    $(`<li class="page-num page-num${nextPage}" data-page=${nextPage}>${nextPage}</li>`).insertAfter($(prevElement));
+                }
+                prevElement = $(prevElement).parent().find(`li.page-num${nextPage}`);
+                count++;
+                // 畫面上頁數最多顯示 5 筆
+                if(count == 5){
+                    break;
+                }
+            }
+        }
+        // 更新 data-page
+        $(".page-prev").attr("data-page", pageNow - 1);
+        $(".page-next").attr("data-page", pageNow + 1);
+        // 若目前頁數為 1，不可再向前翻頁
+        if(pageNow == 1) {
+            $(".page-prev").addClass("page-not-active");
+        } else {
+            $(".page-prev").removeClass("page-not-active");
+        }
+        // 若目前頁數為所有頁數，不可再向後翻頁
+        if(pageNow == pageAll) {
+            $(".page-next").addClass("page-not-active");
+        } else {
+            $(".page-next").removeClass("page-not-active");
+        }
+        $(".table-page").addClass("visible");
+    }
+
+    $(".category li").on("click", function () {
+        $(".category li").removeClass("category-active");
+        $(this).addClass("category-active");
     });
 
     $(".search-btn").on("click", function () {
@@ -40,7 +161,7 @@ $(document).ready(function () {
         // 移除所有 sort 畫面顯示
         $("table i").removeClass("sort-active");
         // 隱藏頁碼顯示
-        $(".tablePage").css("visibility", "hidden");
+        $(".table-page").removeClass("visible");
         // 抓取資料
         axios.get('https://hexschool.github.io/js-filter-data/data.json')
         .then(function(response){
@@ -52,12 +173,13 @@ $(document).ready(function () {
             }
         });
         // 調整畫面顯示：資料載入中
-        showList("loading");
+        showTable("loading");
     });
 
     $("table th").on("click", function (e) {
         // 未讀取資料，不做排序
         if(!dataLoadOK) { return; }
+
         let th;
         if(e.target.nodeName === "I") {
             th = $(e.target).parent();
@@ -67,8 +189,7 @@ $(document).ready(function () {
         let sortUpElement = th.find(".fa-sort-up");
         let sortDownElement = th.find(".fa-sort-down");
         let sortUp = $(sortUpElement).hasClass("sort-active");
-        // let sortDown = $(sortDownElement).hasClass("sort-active");
-        // 移除所有 sort 畫面顯示
+        // 移除所有 sort 效果
         $("table i").removeClass("sort-active");
         if(sortUp) { // 調整成降冪排序
             filterData.sort(function(a, b) {
@@ -82,157 +203,31 @@ $(document).ready(function () {
             $(sortUpElement).addClass("sort-active");
         }
         pageNow = 1;
-        showList("done");
+        showTable("done");
     });
 
-    function dealData(data) {
-        tableData = data;
-        
-        let type = $(".type .type-active").text();
-        let name = $(".search form input[type=text]").val().trim();
-        let checkList = typeList.find(x => x["種類"] == type)["代碼"];
-        filterData = tableData.filter(function(item, index, array) {
-            let checkItem = false;
-            if(checkList.indexOf(item["種類代碼"]) != -1) {
-                if(name == "") {
-                    checkItem = true;
-                } else {
-                    if(item['作物名稱'].indexOf(name) != -1) {
-                        checkItem = true;
-                    }
-                    $(".search form input[type=text]").val("");
-                }
-            }
-            return checkItem;
-        });
-        // console.log(tableData.length, filterData.length);
-        pageAll = Math.ceil(filterData.length / onePageNum);
-        pageNow = pageAll > 0 ? 1 : 0;
-
-        showList("done");
-    }
-
-    function showList(staus) {        
-        let listItem = '';
-        $(list).find("tr").remove(".list-item");
-        if(staus == "loading") {
-            // 更新表格顯示資訊：資料載入中
-            listItem = `<tr class="list-item"><td colspan="${tableHeader.length}">資料載入中...</td></tr>`;
-            // 更新 h2 文字
-            let type = $(".type .type-active").text();
-            let name = $(".search form input[type=text]").val().trim();
-            if(name == "") {
-                $("h2 span").text(`「${type}」的`);
-            } else {
-                $("h2 span").text(`「${name}」的`);
-            }
-            $("h2 span").css("display", "block");
-        } else {
-            if(pageAll == 0) {
-                // 更新表格顯示資訊：查無資料
-                listItem = `<tr class="list-item"><td colspan="${tableHeader.length}">查詢不到當日的交易資訊QQ</td></tr>`;
-                // console.log("1", "windowWidth", windowWidth);
-            } else {
-                let count = 0;
-                let i = (pageNow - 1) * onePageNum;
-                for(; i < filterData.length && count < onePageNum; i++) {
-                    listItem += `<tr class="list-item">`;
-                    let cropName = filterData[i]['作物名稱'];
-                    if(windowWidth <= 568) {
-                        let cropNameArr = cropName.split('-');
-                        listItem += `<td class="font-bold">${cropNameArr[0]}</td>`;
-                    } else {
-                        listItem += `<td class="font-bold">${cropName}</td>`;
-                    }
-                    listItem += `<td class="font-bold">${filterData[i]['市場名稱']}</td>`;
-                    listItem += `<td>${filterData[i]['上價']}</td>`;
-                    listItem += `<td>${filterData[i]['中價']}</td>`;
-                    listItem += `<td>${filterData[i]['下價']}</td>`;
-                    listItem += `<td>${filterData[i]['平均價']}</td>`;
-                    listItem += `<td>${filterData[i]['交易量']}</td>`;
-                    listItem += `</tr>`;
-                    count++;
-                }
-                showPage();
-                // console.log("2", "windowWidth", windowWidth);
-            }
-        }
-        $(list).append(listItem);
-    }
-    
-    function showPage() {
-        let prevElement = $(".prev");
-        $(".tablePage li").remove(".page-num");
-        // console.log("now", pageNow, typeof pageNow, "all", pageAll);
-        let count = 0;
-        for(let i = -2; i < 5; i++) {
-            let nextPage = pageNow + i;
-            // console.log("nextPage", nextPage);
-            if(nextPage >= 1 && nextPage <= pageAll) {
-                // nextPage++;
-                if(nextPage == pageNow){
-                    $(`<li class="page-num page-num${nextPage} page-active" data-page=${nextPage}>${nextPage}</li>`).insertAfter($(prevElement));
-                }
-                else{
-                    $(`<li class="page-num page-num${nextPage}" data-page=${nextPage}>${nextPage}</li>`).insertAfter($(prevElement));
-                }
-                prevElement = $(prevElement).parent().find(`li.page-num${nextPage}`);
-                count++;
-                if(count == 5){
-                    break;
-                }
-            }
-        }
-        // console.log("now", pageNow, typeof pageNow, "all", pageAll);
-        $(".prev").attr("data-page", pageNow - 1);
-        $(".next").attr("data-page", pageNow + 1);
-        if(pageNow == 1) {
-            $(".prev").addClass("page-not-active");
-        } else {
-            $(".prev").removeClass("page-not-active");
-        } 
-        if(pageNow == pageAll) {
-            $(".next").addClass("page-not-active");
-        } else {
-            $(".next").removeClass("page-not-active");
-        }
-        $(".tablePage").css("visibility", "visible");
-    }
-
-    $(".tablePage").on("click", function (e) {
+    $(".table-page").on("click", function (e) {
         let element;
         if(e.target.nodeName === "I"){
             element = $(e.target).parent();
-        } else {
+        } else if(e.target.nodeName === "LI") {
             element = $(e.target);
+        } else {
+            return;
         }
         if($(element).hasClass("page-not-active") || $(element).hasClass("page-active")) {
             return;
         }
         pageNow = parseInt($(element).attr("data-page"));
-        showList("done");
+        showTable("done");
     });
 
-    function test(check, page) {
-        let times = 1;
-        let count = 0;
-        let temp = [];
-        $.each(filterData, function (index, element) {
-            if(times > page) {
-                return false;
-            } else if(times < page){
-                console.log(index);
-            } else {
-                if(element["種類代碼"] == check) {
-                    temp.push(element["作物名稱"]);
-                }
+    $(document).on('keypress',function(e) {
+        console.log(e.which);
+        if(e.which == 13) { // enter
+            if($(".crop-name input[type='text']").is(":focus")) {
+                $(".search-btn").click();
             }
-            count++;
-            if(count >= onePageNum){
-               count = 0;
-               times++;
-            }
-        });
-        console.log(temp);
-    }
+        }
+    });
 });
